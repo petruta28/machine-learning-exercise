680d14609de571a98033681941ecfa35c1ffe662
Petruta <petruta.georgiu@paddypowerbetfair.com>
2018-04-04 14:06:31 +0300

    enron

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -29,28 +29,13 @@ def remove_initials(name):
 
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
-print(len(enron_data))
-print(len(enron_data["SKILLING JEFFREY K"]))
-for key in enron_data:
-    print key, enron_data[key]["poi"]
+# print(len(enron_data))
+# print(len(enron_data["SKILLING JEFFREY K"]))
+# for key in enron_data:
+#     print key, enron_data[key]
+# print("James Prentice total stock value: ", enron_data["PRENTICE JAMES"]["total_stock_value"])
+# print ("Nr of emails from Wesley Colwell to POIs: ", enron_data["COLWELL WESLEY"]["from_this_person_to_poi"])
 
-new_dict = {}
-for key in enron_data:
-    if enron_data[key]["poi"]:
-        new_dict[remove_initials(key)] = enron_data[key]
-
-print(len(new_dict.keys()))
-# print(names)
-
-with open("../final_project/poi_names.csv", "r") as infile:
-    reader1 = csv.reader(infile)
-
-    summ1 = sum(1 for row in reader1 if ((row[0][3:] + row[1]).strip().upper() in new_dict))
-    # for row in reader1:
-    #     print((row[0][3:] + row[1]).strip())
-print(summ1)
-
-
-
+print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
 
 
